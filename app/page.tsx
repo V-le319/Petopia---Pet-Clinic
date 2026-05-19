6ba19c371a17c645fb4aeb20fce988595ba91099
V-le319 <200332747+V-le319@users.noreply.github.com>
2026-05-19 22:35:15 +0700

    fresh setup

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className="main_container">
+    <h1 className="heading">Petopia</h1>
+    </div>
+    </>
+  )
+}
+
+export default Home
