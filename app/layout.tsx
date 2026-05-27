76b3bb838239b4347f8bcf41ab8c7988366a5bb3
V-le319 <200332747+V-le319@users.noreply.github.com>
2026-05-27 14:51:10 +0700

    fixing bg-img

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={cn("font-sans", geist.variable)}>
-      <body className="relative min-h-screen bg-[url('/images/wallpaper.jpg')] bg-cover bg-center bg-fixed">
+      <body className="relative min-h-screen bg-[url('/images/wallpaper.jpg')] bg-repeat bg-auto bg-fixed">
         <Providers> 
         {children}
         <Toaster />
